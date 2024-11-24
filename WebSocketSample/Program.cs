f956068440ac78b108630cd24ff5002c6f99cecd
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-11-25 01:18:44 +0330

    first

---
+++ WebSocketSample/Program.cs
@@ -0,0 +1,55 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseWebSockets();
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+        while (true)
+        {
+            var message = "server response";
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            if (ws.State == WebSocketState.Open)
+                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+            {
+                break;
+            }
+            Thread.Sleep(1000);
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
+    }
+});
+
+app.Run();
